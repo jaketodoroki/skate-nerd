91ec0dd114231f93b28ae07aae92c91473798596
JakeTodoroki <taco7897@gmail.com>
2023-01-17 15:13:08 -0700

    add reviewSchema to skatespot model

--- models/skatespot.js
+++ models/skatespot.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose"
 
 const Schema =mongoose.Schema
 
+const reviewSchema = new Schema({
+  content: String,
+  reviewer: {type: Schema.Types.ObjectId, ref: 'Profile'},
+})
+
 const skatespotSchema = new Schema({
   name: String,
   bust: Boolean,
-  owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  reviews: [reviewSchema] 
 },{
   timestamps: true
 })
